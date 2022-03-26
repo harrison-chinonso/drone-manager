97c6ef4ebf2e0e0d9e770e95f7a353d4079f11bf
beuhier <shianett@gmail.com>
2022-03-26 21:16:37 +0100

    Add main class to project

---
+++ src/main/java/chinonsoharrison/ApplicationMain.java
@@ -0,0 +1,14 @@
+package chinonsoharrison;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMain.class, args);
+	}
+
+}
