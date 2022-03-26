8c7f7dbb5724b2fd64ae7cb386dcc9f502bf91e1
beuhier <shianett@gmail.com>
2022-03-26 22:44:48 +0100

    Update error message handler

--- src/main/java/chinonsoharrison/messages/ErrorMessages.java
+++ src/main/java/chinonsoharrison/messages/ErrorMessages.java
@@ -3,9 +3,7 @@ package chinonsoharrison.messages;
 public enum ErrorMessages {
 
 	INTERNAL_SERVER_ERROR("internal server error. please contact the administrator"),
-	NO_RECORD_FOUND("transaction could not be found"),
-	COULD_NOT_UPDATE_RECORD("transaction could not be updated"),
-	COULD_NOT_DELETE_RECORD("transaction could not be deleted"),
+	NO_RECORD_FOUND("object could not be found"),
 	COULD_NOT_LOAD_MEDICATION("loading medication process could not be completed");
 	
 	private String errorMessages;
